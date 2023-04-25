90368d3f8ee9e80a5ff68d2f29772e1038131721
Nathan <whipmiester@hotmail.com>
2023-04-24 22:16:01 -0400

    first commit

--- src/main/java/edu/iu/c322/demo/orderservice/controller/OrderController.java
+++ src/main/java/edu/iu/c322/demo/orderservice/controller/OrderController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
